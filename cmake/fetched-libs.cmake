5b287ec090cd1f21127ec7176dc2064a6f5fa71c
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2025-10-04 00:06:10 +0700

    Update qoipp dependency

--- cmake/fetched-libs.cmake
+++ cmake/fetched-libs.cmake
@@ -5,7 +5,8 @@ include(FetchContent)
 FetchContent_Declare(
     qoipp
     GIT_REPOSITORY https://github.com/mrizaln/qoipp
-    GIT_TAG v0.5.0
+    GIT_TAG af7534fb526532c4ef7bc975ec297d0d08443487
+    GIT_PROGRESS TRUE
 )
 FetchContent_MakeAvailable(qoipp)
 add_library(fetch::qoipp ALIAS qoipp)
